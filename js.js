437c1b71e4de538cb86f11e165a41d502d674f0e
tal <mrwcggr121@gmail.com>
2023-07-07 17:58:16 +0400

    cooooollllll

--- js.js
+++ js.js
@@ -3,9 +3,12 @@ const logoimage = document.getElementById('mainLogo')
 function hideBlurbox() {
     blurboxDiv.classList.toggle('hidden')
     logoimage.style.width = '10em';
+    logoimage.style.transform='scale(1) translateY(0%)';
+    blurboxDiv.style.backgroundColor= '#00000000';
 }
 
 setTimeout(hideBlurbox, 500);
 setTimeout(() => {
-    blurboxDiv.style.zIndex = "-100";
+    blurboxDiv.style.zIndex= "-100";
+
 }, 1000);
