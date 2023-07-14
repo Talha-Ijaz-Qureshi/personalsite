b97b1473fb4df96e149a5a00368d12aa6374a6aa
tal <mrwcggr121@gmail.com>
2023-07-14 18:31:56 +0400

    new themes

--- js.js
+++ js.js
@@ -12,3 +12,20 @@ setTimeout(() => {
     blurboxDiv.style.zIndex= "-100";
 
 }, 1000);
+
+const buttons = document.getElementsByClassName('themeButton');
+const elements = document.querySelectorAll('body, h1, h2, h3, h4, h5, h6, p, div, a, img, footer'); // Add other elements here
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => {
+    const theme = 'theme' + (i + 1);
+    applyTheme(theme, elements);
+  });
+}
+
+function applyTheme(theme, elements) {
+  elements.forEach((element) => {
+    element.className = element.className.replace(/theme\d+/g, '');
+    element.classList.add(theme);
+  });
+}
